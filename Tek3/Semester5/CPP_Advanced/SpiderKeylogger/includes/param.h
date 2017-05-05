a141ba0377a836f95b289a81035a7398c87e7166
Galdan <g.moulinneuf@outlook.fr>
2017-05-05 14:24:28 +0100

    Added SpiderKeyLogger

---
+++ Tek3/Semester5/CPP_Advanced/SpiderKeylogger/includes/param.h
@@ -0,0 +1,12 @@
+//
+// Created by Galdan on 26/02/2017.
+//
+
+#ifndef SPIDERKEYLOGGER_PARAM_H
+# define SPIDERKEYLOGGER_PARAM_H
+
+typedef struct  s_param {
+    int lon;
+}               t_param;
+
+#endif //SPIDERKEYLOGGER_PARAM_H
---
+++ Tek3/Semester5/CPP_Advanced/SpiderKeylogger/src/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Galdan on 03/01/2017.
+//
+
+#include <boost>
+#include <stdio.h>
+#include "param.h"
+
+int main(int argc, char **argv) {
+    t_param p;
+    p.lon = 43;
+    printf("Hello World ! %d\n", p.lon);
+    return (0);
+}
